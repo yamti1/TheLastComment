c5a1eebc7b6f20f6d7e34aa34bdb4a045d3d0404
unknown <yam.tirosh@gmail.com>
2018-10-19 18:23:12 +0300

    Write function get_my_id

--- the_last_comment.py
+++ the_last_comment.py
@@ -10,8 +10,31 @@ def get_last_commenter_id():
     return ""
 
 
-def get_my_id():
-    return ""
+def get_my_id(user_token):
+    """
+    Returns the user ID of the user associated with the given token.
+    :param user_token: A string. The token of the user.
+    :return: A string. The ID of the user.
+    :raises: RuntimeError if something goes wrong.
+    """
+
+    try:
+        response = requests.get(
+            FACEBOOK_API_URL.format(target="me"),
+            {"access_token": user_token}
+        )
+
+    except ConnectionError as e:
+        msg = "Could not get user's ID because of {}".format(str(e))
+        raise RuntimeError(msg)
+
+    if response.status_code != requests.codes["OK"]:
+        msg = "Could not get user's ID because of response code {}".format(response.status_code)
+        raise RuntimeError(msg)
+
+    json_response = json.loads(response.text)
+
+    return json_response["id"]
 
 
 def comment():
@@ -20,7 +43,7 @@ def comment():
 
 def main():
     while True:
-        if get_last_commenter_id() != get_my_id():
+        if get_last_commenter_id() != get_my_id(USER_TOKEN):
             try:
                 comment()
             except:
