0266b5392321db73bdda73dac9282c48f9b1358f
yam t <yam.tirosh@gmail.com>
2021-06-12 20:33:51 +0300

    WIP

--- the_last_comment.py
+++ the_last_comment.py
@@ -2,11 +2,11 @@ import requests
 import json
 from time import sleep
 
-FACEBOOK_API_URL = "https://graph.facebook.com/{target}?access_token={access_token}"
+FACEBOOK_API_URL = "https://graph.facebook.com/{target}"
 USER_TOKEN = "EAAEOuHUSdj0BAFVnpOZB4yWlgkX6dcMUMcCzRJZCGlRelhzD4bBTPf94MOA9eim4c8ZA95fpmiWcXfVN4F0LhDt0bjTehotiwYdbZCmEGKGGGXRi06t2y7I2atAZA3SvRdFjLig1RxQGJR1XHDCuZBCJLYDbddIaG0E6qAqeU3RlPY5OY2kFVqTLy2F3s4g858Ue14ZC9KzcAZDZD"
 
 
-def get_last_commenter_id():
+def get_last_commenter_id(post_id):
     return ""
 
 
